9f811b64a8bb4205b0175083bc3b992da4c2268b
dyb881 <15118995517@163.com>
2019-10-21 16:20:03 +0800

    添加 demo

---
+++ demo/pc-mobx-admin/precache-manifest.8d44dcf6120b2dd7170dbc602376d9cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec78820b933c62efa6e526d6c53b8bf",
+    "url": ".index.html"
+  },
+  {
+    "revision": "2e05a2e6241f04a0567c",
+    "url": ".static/css/2.4db44991.chunk.css"
+  },
+  {
+    "revision": "24652670b491568a3c55",
+    "url": ".static/css/main.99c41838.chunk.css"
+  },
+  {
+    "revision": "2e05a2e6241f04a0567c",
+    "url": ".static/js/2.32e903ea.chunk.js"
+  },
+  {
+    "revision": "7152e6bf41f404e3f7eb",
+    "url": ".static/js/3.0288ecb2.chunk.js"
+  },
+  {
+    "revision": "24652670b491568a3c55",
+    "url": ".static/js/main.12fe5451.chunk.js"
+  },
+  {
+    "revision": "532b27c2dbd59463d184",
+    "url": ".static/js/runtime-main.a5fc717c.js"
+  }
+]);
