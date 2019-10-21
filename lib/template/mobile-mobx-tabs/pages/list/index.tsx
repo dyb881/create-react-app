2e79cb1e29dda222972d1e00bd4833b5ea958dcb
dyb881 <15118995517@163.com>
2019-10-21 17:01:57 +0800

    添加 demo

--- lib/template/mobile-mobx-tabs/pages/list/index.tsx
+++ lib/template/mobile-mobx-tabs/pages/list/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Icon } from 'antd-mobile';
 import { TRoute } from 'types';
+import style from './style.module.less';
 
 export default class extends React.Component<TRoute> {
   render() {
     return (
-      <div>
-        <span onClick={this.props.history.goBack}>
-          <Icon type="left" />
-        </span>
-        列表
+      <div className={`fill ${style.box}`}>
+        <div className={`center ${style.header}`}>
+          <span className={`center ${style.left}`} onClick={this.props.history.goBack}>
+            <Icon type="left" />
+          </span>
+          列表
+        </div>
+        列表页
       </div>
     );
   }
